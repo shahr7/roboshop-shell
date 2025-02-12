eae4fa2e70a095e92cd88eef8ed39df75958efd9
shahr <shah.randeep16@gmail.com>
2025-02-12 10:49:28 -0600

    commiting frontend

---
+++ catalogue.sh
@@ -0,0 +1,24 @@
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+useradd roboshop
+
+rm -rf /app
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+cp catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-mongosh -y
+mongosh --host mongodb-dev.azdevops87.online </app/db/master-data.js
