953e5852fda81854b3e7ee4c82ace79afc01b58f
shahr <shah.randeep16@gmail.com>
2025-02-12 10:21:11 -0600

    commiting frontend

--- frontend.sh
+++ frontend.sh
@@ -6,10 +6,3 @@ cp nginx.conf /etc/nginx/nginx.conf
 systemctl enable nginx
 systemctl start nginx
 
-rm -rf /usr/share/nginx/html/*
-
-rm -rf /tmp/frontend.zip
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
-systemctl restart nginx
